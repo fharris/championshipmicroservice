48242aa3e04d146b549547bdbd39e3abfa0b315e
fharris <fernando.harris@oracle.com>
2024-04-30 13:15:58 +0200

    Update deploy-championship.sh

--- deploy-championship.sh
+++ deploy-championship.sh
@@ -9,7 +9,7 @@ echo "############################"
 echo "############################"
 echo "############################"
 echo "############################"
-kubectl apply -f appconfig/championshipms-namespace.yaml;
+kubectl apply -f appconfig/consumerms-namespace.yaml;
 kubectl apply -f ./databaseconfig/.
 echo ".... waiting for MySQL pod to run to run db configuration...Deploying Championship Microservice"
 echo "############################"
@@ -20,7 +20,7 @@ kubectl -n curiosityevents exec -it  `kubectl -n curiosityevents get \
  -- mysql -h 127.0.0.1 -u root -pmySQLpword#2023 < ./databaseconfig/create-championshipdb-resources.sql;
 kubectl apply -f ./appconfig/. ;
 echo "updating for a public accessible image of the application"
-kubectl -n curiosityevents set image deployment/championshipms-deployment consumerms=fharris/consumerms:latest
+kubectl -n curiosityevents set image deployment/consumerms-deployment consumerms=fharris/consumerms:latest
 echo ".... waiting for the application to get deployed..."
 sleep 20;
 kubectl get pods -n curiosityevents;
