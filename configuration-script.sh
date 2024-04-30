93fa9198851327b98ff753e26281e39ecce6cf67
fharris <fernando.harris@oracle.com>
2024-04-30 13:14:31 +0200

    Update configuration-script.sh

--- configuration-script.sh
+++ configuration-script.sh
@@ -7,11 +7,11 @@ echo Running in $SHELL
 # 1.	Get the code from Github
 # If you haven’t done yet, get the code from the repository:
 
-## git clone https://github.com/fharris/curiositymonolith
+## git clone https://github.com/fharris/championshipmicroservice
 
-# 2.	Create the curiositymonolith namespace
+# 2.	Create the curiosityevents namespace
 
-kubectl apply -f championshipms-namespace.yaml
+kubectl apply -f consumerms-namespace.yaml
 
 # 3.	Deploy database
 
@@ -25,20 +25,20 @@ kubectl apply -f ./databaseconfig/mysql-persistent-deploy.yaml
 kubectl apply -f ./databaseconfig/mysql-db-service.yaml
 
 kubectl -n curiosityevents \ 
-create secret generic championshipms-mysql-db-secret \ 
+create secret generic consumerms-mysql-db-secret \ 
 --from-literal=SPRING_DATASOURCE_PASSWORD=$MYSQL_CREDENTIALS_PSW \
 --from-literal=SPRING_DATASOURCE_USERNAME=$MYSQL_CREDENTIALS_USR
 
-# Replace $MYSQL_CREDENTIALS_PSW with the password like Welcome#1 and $MYSQL_CREDENTIALS_USR with the user which should be “curiosity”.
+# Replace $MYSQL_CREDENTIALS_PSW with the password like Welcome#1 and $MYSQL_CREDENTIALS_USR with the user which should be “championship”.
 
 # When the mysql pod is running configure the mysql database for the application:  
 
-# kubectl -n curiositymonolith port-forward svc/mysql-db-deployment 3306:80
+# kubectl -n curiosityevents port-forward svc/mysql-db-deployment 3306:80
 
-# And in a different tab, go to where you’ve clone the repository, and run the following command to create the user curiosity and the database curiositydb:
+# And in a different tab, go to where you’ve clone the repository, and run the following command to create the user championship and the database championshipdb:
 #(You need to have a mysql client installed, change the password accordingly to what you used in step 333.,m)
 
-#mysql -h 127.0.0.1 -u root -pmySQLpword#2023 < ./ databaseconfig/create-curiositydb-resources.sql
+#mysql -h 127.0.0.1 -u root -pmySQLpword#2023 < ./ databaseconfig/create-championshipdb-resources.sql
 
 #If you want to validate, run :
 
@@ -47,19 +47,19 @@ create secret generic championshipms-mysql-db-secret \
 
 #SHOW DATABASES;
 
-#You should be able to see the curiositydb created. 
+#You should be able to see the championshipdb created. 
 
 #4.	Deploy the application
 
-kubectl apply -f ./appconfig/championshipms-service-clusterip.yaml
-kubectl apply -f ./appconfig/championshipms-configmap.yaml
-kubectl describe configmap championshipms-configmap -n curiosityevents
-kubectl apply -f  ./appconfig/championshipms-deployment.yaml -n curiosityevents
-#kubectl rollout restart -f curiositymonolith-deployment.yaml
+kubectl apply -f ./appconfig/consumerms-service-clusterip.yaml
+kubectl apply -f ./appconfig/consumerms-configmap.yaml
+kubectl describe configmap consumerms-configmap -n curiosityevents
+kubectl apply -f  ./appconfig/consumerms-deployment.yaml -n curiosityevents
+#kubectl rollout restart -f consumerms-deployment.yaml
 kubectl get deployments -n curiosityevents
 kubectl get pods -n curiosityevents
 
-#You should be able to see the curiositymonolith and respective database pods running. Lets access it with:
-#kubectl -n curiositymonolith port-forward svc/curiositymonolith-service-lb 9000:80
+#You should be able to see the championship microservice and respective database pods running. Lets access it with:
+#kubectl -n curiosityevents port-forward svc/consumerms-service-clusterip 9000:80
 
 
