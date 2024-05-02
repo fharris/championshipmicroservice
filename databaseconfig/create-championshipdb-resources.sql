05dc744b757d7ffac78191879e38f5b6bcd39087
Fernando Harris <fernando.harris@oracle.com>
2024-05-02 09:58:17 +0200

    same user for both db

--- databaseconfig/create-championshipdb-resources.sql
+++ databaseconfig/create-championshipdb-resources.sql
@@ -1,7 +1,10 @@
 DROP DATABASE IF EXISTS championshipdb;
 DROP USER IF EXISTS championship;
 CREATE DATABASE championshipdb;
-CREATE USER 'championship'@'%' IDENTIFIED BY 'Welcome#1';
-GRANT ALL PRIVILEGES ON championshipdb.* TO 'championship'@'%'; 
+#CREATE USER 'championship'@'%' IDENTIFIED BY 'Welcome#1';
+#GRANT ALL PRIVILEGES ON championshipdb.* TO 'championship'@'%'; 
+
+#CREATE USER 'championship'@'%' IDENTIFIED BY 'Welcome#1';
+GRANT ALL PRIVILEGES ON championshipdb.* TO 'curiosity'@'%'; 
 SHOW DATABASES;
 
