a6e5113bd5b6d4a7a1b6ba691ad1517df77e8684
fharris <fernando.harris@oracle.com>
2024-05-07 13:54:46 +0200

    Update championshipconfig.groovy

--- jenkinsconfig/championshipconfig.groovy
+++ jenkinsconfig/championshipconfig.groovy
@@ -39,7 +39,7 @@ pipeline {
 	          sh "kubectl apply -f appconfig/consumerms-namespace.yaml"
             sh "kubectl apply -f ./databaseconfig/."
             sh "sleep 30"
-            sh "kubectl -n curiosityevents exec -it `kubectl -n curiosityevents get --no-headers=true pods -l app=mysql-db -o custom-columns=:metadata.name` -- mysql -h 127.0.0.1 -u root -pmySQLpword#2023 < ./databaseconfig/create-championshipdb-resources.sql"
+            //sh "kubectl -n curiosityevents exec -it `kubectl -n curiosityevents get --no-headers=true pods -l app=mysql-db -o custom-columns=:metadata.name` -- mysql -h 127.0.0.1 -u root -pmySQLpword#2023 < ./databaseconfig/create-championshipdb-resources.sql"
             sh 'kubectl -n curiosityevents create secret generic consumerms-mysql-db-secret --from-literal=SPRING_DATASOURCE_PASSWORD=$MYSQL_CREDENTIALS_PSW --from-literal=SPRING_DATASOURCE_USERNAME=$MYSQL_CREDENTIALS_USR --dry-run=client -o yaml > consumerms-mysql-db-secret.yaml'
             sh "kubectl apply -f consumerms-mysql-db-secret.yaml"
             sh "kubectl apply -f ./appconfig/."
